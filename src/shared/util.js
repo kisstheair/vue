461c4a7a4df8c4bb58ef6506dc24641654b9584c
kisstheair <fighting20xx@outlook.com>
2017-10-12 15:33:52 +0800

    注释 shared 下面的 util 工具函数

--- src/shared/util.js
+++ src/shared/util.js
@@ -21,13 +21,13 @@ export function toNumber (val: string): number | string {
 }
 
 /**
- * Make a map and return a function for checking if a key
- * is in that map.
+ * Make a map and return a function for checking if a key            制造一个map，保存初始值，来对比下次传入的值是否已经有了。
+ * is in that map.                                                    构建功能性函数的函数，  根据传入的值生成一个对象，  当生成的函数执行的时候，传入的值和对象中的数对比，有则返回true。
  */
 export function makeMap (
   str: string,
   expectsLowerCase?: boolean
-): (key: string) => true | void {
+): (key: string) => true | void {                           //泛型的用法，  返回值是一个函数，  这个函数的传入值是字符串
   const map = Object.create(null)
   const list: Array<string> = str.split(',')
   for (let i = 0; i < list.length; i++) {
@@ -41,10 +41,10 @@ export function makeMap (
 /**
  * Check if a tag is a built-in tag.
  */
-export const isBuiltInTag = makeMap('slot,component', true)
+export const isBuiltInTag = makeMap('slot,component', true)        // 返回的是一个函数isBuiltInTag，  这个函数 传入 solt  会返回true， 传入没有的返回 undefined
 
 /**
- * Remove an item from an array
+ * Remove an item from an array                                       // 把元素从数组中移除
  */
 export function remove (arr: Array<any>, item: any): Array<any> | void {
   if (arr.length) {
@@ -58,20 +58,20 @@ export function remove (arr: Array<any>, item: any): Array<any> | void {
 /**
  * Check whether the object has the property.
  */
-const hasOwnProperty = Object.prototype.hasOwnProperty
+const hasOwnProperty = Object.prototype.hasOwnProperty                // 检测 是自己的属性，还是原型上的属性。
 export function hasOwn (obj: Object, key: string): boolean {
   return hasOwnProperty.call(obj, key)
-}
+}      // 判断是不是自己的属性。
 
 /**
- * Check if value is primitive
+ * Check if value is primitive                                         // 判断是不是原始属性，   ECMAScript 有 5 种原始类型（primitive type），即 Undefined、Null、Boolean、Number 和 String。
  */
 export function isPrimitive (value: any): boolean {
   return typeof value === 'string' || typeof value === 'number'
 }
 
 /**
- * Create a cached version of a pure function.
+ * Create a cached version of a pure function.                      // 制造的是一个个缓存系统 的函数。   制造出来的函数 传入一个值 处理一下，备份之后再返回，  下次在用到 先查询备份的有没有。
  */
 export function cached (fn: Function): Function {
   const cache = Object.create(null)
@@ -82,7 +82,7 @@ export function cached (fn: Function): Function {
 }
 
 /**
- * Camelize a hyphen-delmited string.
+ * Camelize a hyphen-delmited string.                                  // 把横线分隔的转换为驼峰函数。camelize（a-b-c） 返回ABC  然后缓存起来。
  */
 const camelizeRE = /-(\w)/g
 export const camelize = cached((str: string): string => {
@@ -90,14 +90,14 @@ export const camelize = cached((str: string): string => {
 })
 
 /**
- * Capitalize a string.
+ * Capitalize a string.                                                  // 首字符大写
  */
 export const capitalize = cached((str: string): string => {
   return str.charAt(0).toUpperCase() + str.slice(1)
 })
 
 /**
- * Hyphenate a camelCase string.
+ * Hyphenate a camelCase string.                                        // 把驼峰式写法 转换为  连字符写法。
  */
 const hyphenateRE = /([^-])([A-Z])/g
 export const hyphenate = cached((str: string): string => {
@@ -108,7 +108,7 @@ export const hyphenate = cached((str: string): string => {
 })
 
 /**
- * Simple bind, faster than native
+ * Simple bind, faster than native                                      // 创建在指定环境中运行的 函数   ，
  */
 export function bind (fn: Function, ctx: Object): Function {
   function boundFn (a) {
@@ -205,7 +205,7 @@ export function genStaticKeys (modules: Array<ModuleOptions>): string {
 
 /**
  * Check if two values are loosely equal - that is,
- * if they are plain objects, do they have the same shape?
+ * if they are plain objects, do they have the same shape?            判断2个数据是否 一样
  */
 export function looseEqual (a: mixed, b: mixed): boolean {
   const isObjectA = isObject(a)
@@ -219,7 +219,7 @@ export function looseEqual (a: mixed, b: mixed): boolean {
   }
 }
 
-export function looseIndexOf (arr: Array<mixed>, val: mixed): number {
+export function looseIndexOf (arr: Array<mixed>, val: mixed): number {       // 判断 一个值在数组中的位置。
   for (let i = 0; i < arr.length; i++) {
     if (looseEqual(arr[i], val)) return i
   }
